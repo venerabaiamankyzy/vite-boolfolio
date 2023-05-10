3f42692f9952ec27c6dbed6075f1b2d29b57bcc5
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-05-10 17:02:35 +0200

    add bootstrap & check

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+// import AppHeader from "./components/AppHeader.vue";
+export default {
+  data() {
+    return {
+      title: "Hello World",
+    };
+  },
+
+  // components: {
+  //   AppHeader,
+  //   AppMain,
+  // },
+};
+</script>
+
+<template>
+  <h1 class="text-danger">
+    {{ title }}
+  </h1>
+</template>
+
+<style lang="scss" scoped></style>
