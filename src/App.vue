648969e30a6c1e89d5721ed3636648fd68df99ce
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-05-10 20:59:40 +0200

    add axios

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,41 @@
 <script>
-// import AppHeader from "./components/AppHeader.vue";
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
 export default {
   data() {
     return {
       title: "Hello World",
+      projects: [],
     };
   },
 
-  // components: {
-  //   AppHeader,
-  //   AppMain,
-  // },
+  methods: {
+    fetchProjects() {
+      axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+        this.projects = response.data;
+      });
+    },
+  },
+
+  created() {
+    this.fetchProjects();
+  },
+
+  components: {
+    AppHeader,
+    // AppMain,
+  },
 };
 </script>
 
 <template>
+  <AppHeader />
   <h1 class="text-danger">
     {{ title }}
   </h1>
+  <ul>
+    <li v-for="project in projects">{{ project.title }}</li>
+  </ul>
 </template>
 
 <style lang="scss" scoped></style>
