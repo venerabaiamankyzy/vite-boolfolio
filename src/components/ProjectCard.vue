5656f7475096ba380830a02728d62fc2f4da342c
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-05-11 21:29:04 +0200

    add ProjectList

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,35 @@
+<script>
+// import MyComponent from "./components/MyComponent.vue";
+
+export default {
+  name: "ProjectCard",
+
+  props: {
+    project: Object,
+  },
+
+  computed: {
+    abstract() {
+      return this.project.text.slice(0, 200) + "...";
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div class="card">
+      <div class="card-header">
+        <h3>{{ project.title }}</h3>
+      </div>
+
+      <div class="card-body">
+        <p>{{ project.text }}</p>
+      </div>
+
+      <div class="card-footer">Created at {{ project.created_at }}</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
