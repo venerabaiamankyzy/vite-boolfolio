9a25d31225e15619416f52312eda8983ad927a88
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-05-11 21:28:48 +0200

    add ProjectList

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,33 @@
+<script>
+import ProjectCard from "./ProjectCard.vue";
+
+export default {
+  name: "ProjectList",
+
+  props: {
+    projects: Array,
+    title: String,
+  },
+
+  components: {
+    ProjectCard,
+  },
+};
+</script>
+
+<template>
+  <section class="container">
+    <h1 class="my-4">{{ title }}</h1>
+    <div v-if="projects.length" class="row g-4">
+      <ProjectCard
+        v-for="project in projects"
+        :key="project.id"
+        :project="project"
+        class="col-md-4 d-flex"
+      />
+    </div>
+    <h2 v-else class="text-muted">Non ci sono progetti</h2>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
